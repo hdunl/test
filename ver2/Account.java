a4c0480f61e916fc9a5123bb86a1bc1bcce1f611
RombertiKDyre22 <54483523+RombertiKDyre22@users.noreply.github.com>
2022-01-15 00:51:59 -0500

    acct.java update

--- ver2/Account.java
+++ ver2/Account.java
@@ -5,10 +5,7 @@ public class Account {
 	// New comment for balance
 	private double balance;
 	private String name;
-	// Constructor
-	public Account(double balance) {
-		this("Unknown", balance);
-	}
+	
 
 	public Account(String name, double balance) {
 		this.name = name;
